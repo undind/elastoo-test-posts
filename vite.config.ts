e5ebb5b0efd82df144698bed701eebdf4fb55329
undind <flyker777@yandex.ru>
2024-04-04 15:00:51 +0300

    gh pages package

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://undind.github.io/elastoo-test-posts',
+  base: '/elastoo-test-posts/',
   plugins: [
     vue(),
     vueJsx(),
